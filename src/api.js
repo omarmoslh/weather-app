fe07ee75fd75edfc19537164a675f845fcab4187
Omar_Mosleh <omoslh@gmail.com>
2023-11-11 11:32:21 +0200

    first commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+
+  headers: {
+    "X-RapidAPI-Key": "ea68a4268fmsh36ecd0de470b95dp1e7fffjsnf3e9264e127b",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "5b7842fb1eef2849520b11da1933ac3b";
